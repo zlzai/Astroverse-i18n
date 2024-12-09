76857584942cb61b3884bc752e78cfedc7ce1cc5
iNote <137146930+inotelab@users.noreply.github.com>
2024-12-09 17:03:22 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://verse-i18n.vercel.app",
-  trailingSlash: "never",
+  trailingSlash: "always",
   prefetch: true,
   i18n: {
     defaultLocale: "en",
